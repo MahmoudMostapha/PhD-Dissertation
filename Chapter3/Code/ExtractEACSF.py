c95453c286114ed770d2d9f5c5c766682efbfefb
Mahmoud Mostapha <36214046+MahmoudMostapha@users.noreply.github.com>
2020-03-23 10:59:17 -0400

    Upload

--- Chapter3/Code/ExtractEACSF.py
+++ Chapter3/Code/ExtractEACSF.py
@@ -38,23 +38,23 @@ if LH:
 
 	subprocess.call('echo Establishing Surface Correspondance', shell=True)
 
-	os.system('../Code/joohwi/klaplace-build/klaplace -dims 300 ' + str(ID) + '_LH_MID.vtk '  + str(ID) + '_LH_GM_Outer_MC.vtk  -surfaceCorrespondence ' + str(ID) + '_LH_Outer.corr')
+	os.system('../Code/SolveLaplacePDE/klaplace-build/klaplace -dims 300 ' + str(ID) + '_LH_MID.vtk '  + str(ID) + '_LH_GM_Outer_MC.vtk  -surfaceCorrespondence ' + str(ID) + '_LH_Outer.corr')
 
 	subprocess.call('Establishing Streamlines', shell=True)
 
-	os.system('../Code/joohwi/klaplace-build/klaplace -traceStream ' + str(ID) +  '_LH_Outer.corr_field.vts ' + str(ID) + '_LH_MID.vtk ' + str(ID) + '_LH_GM_Outer_MC.vtk ' + str(ID) + '_LH_Outer_streamlines.vtp ' + str(ID) + '_LH_Outer_points.vtp -traceDirection forward')
+	os.system('../Code/SolveLaplacePDE/klaplace-build/klaplace -traceStream ' + str(ID) +  '_LH_Outer.corr_field.vts ' + str(ID) + '_LH_MID.vtk ' + str(ID) + '_LH_GM_Outer_MC.vtk ' + str(ID) + '_LH_Outer_streamlines.vtp ' + str(ID) + '_LH_Outer_points.vtp -traceDirection forward')
 
-	os.system('../Code/joohwi/klaplace-build/klaplace -conv ' + str(ID) + '_LH_Outer_streamlines.vtp ' + str(ID) + '_LH_Outer_streamlines.vtk')
+	os.system('../Code/SolveLaplacePDE/klaplace-build/klaplace -conv ' + str(ID) + '_LH_Outer_streamlines.vtp ' + str(ID) + '_LH_Outer_streamlines.vtk')
 
 	subprocess.call('echo Computing LH EACSF', shell=True)
 
-	os.system('../Code/Estimate1DCortexStreamlinesDensity/build/EstimateCortexStreamlinesDensity ' + str(ID) + '_LH_MID.vtk ' + str(ID) + '_LH_Outer_streamlines.vtk ' + str(ID) + '_CSF_Prop_Masked.nrrd ' + str(ID) + '_LH_GM_Dilated.nrrd ' + str(ID) + '_LH_CSF_Density.vtk ' + str(ID) + '_LH_Visitation.nrrd 0 0 0')
+	os.system('../Code/EstimateLocalEACSF/build/EstimateCortexStreamlinesDensity ' + str(ID) + '_LH_MID.vtk ' + str(ID) + '_LH_Outer_streamlines.vtk ' + str(ID) + '_CSF_Prop_Masked.nrrd ' + str(ID) + '_LH_GM_Dilated.nrrd ' + str(ID) + '_LH_CSF_Density.vtk ' + str(ID) + '_LH_Visitation.nrrd 0 0 0')
 
 	os.system('/proj/NIRAL/tools/MeshMath ' + str(ID) + '_LH_GM.vtk ' + str(ID) + '_LH_GM.vtk -KWMtoPolyData ' + str(ID) + '_LH_MID.CSFDensity.txt EACSF')
 
 	os.system('mv ' + str(ID) + '_LH_MID.CSFDensity.txt ' + str(ID) + '_LH_MID.CSFDensity_All.txt ')
 
-	os.system('../Code/Estimate1DCortexStreamlinesDensity/build/EstimateCortexStreamlinesDensity ' + str(ID) + '_LH_MID.vtk ' + str(ID) + '_LH_Outer_streamlines.vtk ' + str(ID) + '_CSF_Prop_Masked_EA.nrrd ' + str(ID) + '_LH_GM_Dilated.nrrd ' + str(ID) + '_LH_CSF_Density.vtk ' + str(ID) + '_LH_Visitation.nrrd 0 0 0')
+	os.system('../Code/EstimateLocalEACSF/build/EstimateCortexStreamlinesDensity ' + str(ID) + '_LH_MID.vtk ' + str(ID) + '_LH_Outer_streamlines.vtk ' + str(ID) + '_CSF_Prop_Masked_EA.nrrd ' + str(ID) + '_LH_GM_Dilated.nrrd ' + str(ID) + '_LH_CSF_Density.vtk ' + str(ID) + '_LH_Visitation.nrrd 0 0 0')
 
 	os.system('/proj/NIRAL/tools/MeshMath ' + str(ID) + '_LH_GM.vtk ' + str(ID) + '_LH_GM.vtk -KWMtoPolyData ' + str(ID) + '_LH_MID.CSFDensity.txt EACSF_EA')
 
@@ -78,23 +78,23 @@ if RH:
 
 	subprocess.call('echo Establishing Surface Correspondance', shell=True)
 
-	os.system('../Code/joohwi/klaplace-build/klaplace -dims 300 ' + str(ID) + '_RH_MID.vtk '  + str(ID) + '_RH_GM_Outer_MC.vtk  -surfaceCorrespondence ' + str(ID) + '_RH_Outer.corr')
+	os.system('../Code/SolveLaplacePDE/klaplace-build/klaplace -dims 300 ' + str(ID) + '_RH_MID.vtk '  + str(ID) + '_RH_GM_Outer_MC.vtk  -surfaceCorrespondence ' + str(ID) + '_RH_Outer.corr')
 
 	subprocess.call('Establishing Streamlines', shell=True)
 
-	os.system('../Code/joohwi/klaplace-build/klaplace -traceStream ' + str(ID) +  '_RH_Outer.corr_field.vts ' + str(ID) + '_RH_MID.vtk ' + str(ID) + '_RH_GM_Outer_MC.vtk ' + str(ID) + '_RH_Outer_streamlines.vtp ' + str(ID) + '_RH_Outer_points.vtp -traceDirection forward')
+	os.system('../Code/SolveLaplacePDE/klaplace-build/klaplace -traceStream ' + str(ID) +  '_RH_Outer.corr_field.vts ' + str(ID) + '_RH_MID.vtk ' + str(ID) + '_RH_GM_Outer_MC.vtk ' + str(ID) + '_RH_Outer_streamlines.vtp ' + str(ID) + '_RH_Outer_points.vtp -traceDirection forward')
 
-	os.system('../Code/joohwi/klaplace-build/klaplace -conv ' + str(ID) + '_RH_Outer_streamlines.vtp ' + str(ID) + '_RH_Outer_streamlines.vtk')
+	os.system('../Code/SolveLaplacePDE/klaplace-build/klaplace -conv ' + str(ID) + '_RH_Outer_streamlines.vtp ' + str(ID) + '_RH_Outer_streamlines.vtk')
 
 	subprocess.call('echo Computing RH EACSF', shell=True)
 
-	os.system('../Code/Estimate1DCortexStreamlinesDensity/build/EstimateCortexStreamlinesDensity ' + str(ID) + '_RH_MID.vtk ' + str(ID) + '_RH_Outer_streamlines.vtk ' + str(ID) + '_CSF_Prop_Masked.nrrd ' + str(ID) + '_RH_GM_Dilated.nrrd ' + str(ID) + '_RH_CSF_Density.vtk ' + str(ID) + '_RH_Visitation.nrrd 0 0 0')
+	os.system('../Code/EstimateLocalEACSF/build/EstimateCortexStreamlinesDensity ' + str(ID) + '_RH_MID.vtk ' + str(ID) + '_RH_Outer_streamlines.vtk ' + str(ID) + '_CSF_Prop_Masked.nrrd ' + str(ID) + '_RH_GM_Dilated.nrrd ' + str(ID) + '_RH_CSF_Density.vtk ' + str(ID) + '_RH_Visitation.nrrd 0 0 0')
 
 	os.system('/proj/NIRAL/tools/MeshMath ' + str(ID) + '_RH_GM.vtk ' + str(ID) + '_RH_GM.vtk -KWMtoPolyData ' + str(ID) + '_RH_MID.CSFDensity.txt EACSF')
 
 	os.system('mv ' + str(ID) + '_RH_MID.CSFDensity.txt ' + str(ID) + '_RH_MID.CSFDensity_All.txt ')
 
-	os.system('../Code/Estimate1DCortexStreamlinesDensity/build/EstimateCortexStreamlinesDensity ' + str(ID) + '_RH_MID.vtk ' + str(ID) + '_RH_Outer_streamlines.vtk ' + str(ID) + '_CSF_Prop_Masked_EA.nrrd ' + str(ID) + '_RH_GM_Dilated.nrrd ' + str(ID) + '_RH_CSF_Density.vtk ' + str(ID) + '_RH_Visitation.nrrd 0 0 0')
+	os.system('../Code/EstimateLocalEACSF/build/EstimateCortexStreamlinesDensity ' + str(ID) + '_RH_MID.vtk ' + str(ID) + '_RH_Outer_streamlines.vtk ' + str(ID) + '_CSF_Prop_Masked_EA.nrrd ' + str(ID) + '_RH_GM_Dilated.nrrd ' + str(ID) + '_RH_CSF_Density.vtk ' + str(ID) + '_RH_Visitation.nrrd 0 0 0')
 
 	os.system('/proj/NIRAL/tools/MeshMath ' + str(ID) + '_RH_GM.vtk ' + str(ID) + '_RH_GM.vtk -KWMtoPolyData ' + str(ID) + '_RH_MID.CSFDensity.txt EACSF_EA')
 
